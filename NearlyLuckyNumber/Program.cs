c035266db33ce2d97faee98785a3ad7cb1aaf3b3
Hassan Aijaz <hassanaijaz233@gmail.com>
2022-10-09 12:31:57 -0700

    110A Nearly Lucky Number

---
+++ NearlyLuckyNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿var input = Console.ReadLine();
+var count = input.Count(c => c == '4' || c == '7');
+if (count == 4 || count == 7)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+} 
