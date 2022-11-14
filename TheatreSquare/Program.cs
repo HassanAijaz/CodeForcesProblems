239123274810bec51511b750f87aed4bb7aba57e
Hassan Aijaz <hassanaijaz233@gmail.com>
2022-11-14 22:25:34 +0500

    A. Theatre Square

---
+++ TheatreSquare/Program.cs
@@ -0,0 +1,18 @@
+﻿var input = Console.ReadLine().Split(' ').Select(c => int.Parse(c)).ToArray();
+int n = input[0];
+int m = input[1];
+int a = input[2];
+
+long len = n / a;
+long bre = m / a;
+if (n % a != 0)
+{
+    len++;
+}
+
+if (m % a != 0)
+{
+    bre++;
+}
+
+Console.WriteLine(len * bre);
